16e227fd948589de256b8b69b825d84d89f56916
xian278 <145316536+christ278@users.noreply.github.com>
2025-03-31 11:18:50 +0800

    Added initial patient details

---
+++ SoCot_HC_FE/Controllers/PatientReceivingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoCot_HC_FE.Controllers
+{
+    public class PatientReceivingController : Controller
+    {
+        // GET: PatientReceiving
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
