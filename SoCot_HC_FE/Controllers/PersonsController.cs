a40a18f6d880c90fcab98d4c118ef97360eec67f
Paul <paulkemon029@gmail.com>
2025-04-12 01:56:05 +0800

    Startup UI and Added Persons

---
+++ SoCot_HC_FE/Controllers/PersonsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoCot_HC_FE.Models;
+
+namespace SoCot_HC_FE.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [HttpGet]
+        public ActionResult List()
+        {
+            //SAMPLE DATA
+            var personList = new List<Person>
+            {
+                new Person
+                {
+                    PersonId = Guid.NewGuid(),
+                    Firstname = "John",
+                    Middlename = "A.",
+                    Lastname = "Doe",
+                    Birthdate = "1990-01-01"
+                },
+                new Person
+                {
+                    PersonId = Guid.NewGuid(),
+                    Firstname = "Jane",
+                    Middlename = "B.",
+                    Lastname = "Smith",
+                    Birthdate = "1985-07-15"
+                }
+            };
+
+            return View(personList);
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SoCot_HC_FE/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SoCot_HC_FE.Models
+{
+    public class Person
+    {
+        public Guid PersonId { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
