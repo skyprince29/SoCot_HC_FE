4d2f8963fd69f1664220f492ac39b51cd37036b1
xian278 <145316536+christ278@users.noreply.github.com>
2025-04-14 13:56:50 +0800

    Update person model

--- SoCot_HC_FE/Models/Person.cs
+++ SoCot_HC_FE/Models/Person.cs
@@ -12,6 +12,19 @@ namespace SoCot_HC_FE.Models
         public string Firstname { get; set; }
         public string Middlename { get; set; }
         public string Lastname { get; set; }
+        public string Suffix { get; set; }
         public string Birthdate { get; set; }
+        public string BirthPlace { get; set; }
+        public string Gender { get; set; }
+        public string CivilStatus { get; set; }
+        public string Religion { get; set; }
+        public string  ContactNo { get; set; }
+        public string Email { get; set; }
+        public Guid AddressIdResidential { get; set; }
+        public Address AddressResidential {  set; get; }
+        public Guid AddressIdPermanent { get; set; }
+        public Address AddressPermanent { set; get; }
+        public bool IsDeceased { get; set; }
+        public bool BloodType { get; set; }
     }
 }
